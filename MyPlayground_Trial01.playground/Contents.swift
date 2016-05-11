1ff22dccf3461cd721b9675fed413e1350f509e4
Christoph Schulz <christoph1@christoph-schulzs-macbook-air.local>
2016-05-11 10:51:37 +0200

    first commit

---
+++ MyPlayground_Trial01.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+//edit for testing github
+
+import Cocoa
+
+// Definition einer ersten Variable
+var string01:String = "Erste Swift-Zeichenfolge"
+
+string01 = "Hallo SwiftBog!"
+
+//Verschiedene Variabeln kombonieren
+var char1 = "C"
+var char2 = "S"
+var char3 = "Z"
+
+var string02:String = char1 + char2 + char3 + string01
+
+//Berechnung mit Hilfe von Swift
+var num01:Int = 10
+var num02:Int = 11
+var num03:Int = 12
+
+var calc01 = num01 + num02 + num03
+
+//Nur Werte ausgeben
+
+string01 + string02
+
+string01 + char1
+
+
+//Implizite Datentypen (ohne Angabe des Datentyps)
+var ganzzahl = 123
+var gleitkommazahl = 12.34567
+var pi = 3.1415
+
+//Explizite Datentypen (mit Angabe des Datentyps)
+var ganzzahlDatentyp:Int = 123
+var gleitkommazahlDatentyp:Float = 12.2234
+var piDatentyp:Double = 3.141597
+
+
+//Testklasse erstellen
+//var christoph:Personen = "Christoph"
